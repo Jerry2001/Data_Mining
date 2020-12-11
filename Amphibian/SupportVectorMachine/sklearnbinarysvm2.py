faf206e31a18979e7b10d51f681cbdf1f7000665
Jerry2001 <ngohongphuc2001@gmail.com>
2020-12-10 21:53:08 -0600

    Add kFold Validation for SVM & restructure

--- Amphibian/SupportVectorMachine/sklearnbinarysvm2.py
+++ Amphibian/SupportVectorMachine/sklearnbinarysvm2.py
@@ -4,6 +4,7 @@ from sklearn import svm
 from sklearn.metrics import accuracy_score 
 from sklearn.metrics import classification_report 
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 
 def accuracyCalc(label, predict):
 	if(not isinstance(label, list)): label = list(label['label'])
@@ -21,20 +22,43 @@ def accuracyCalc(label, predict):
 		#print(u, v, curAccuracy / 7.0)
 	return (match * 1.0 / (len(label) * 7))
 
-trainData = pd.read_csv("../Dataset/binarytrain.csv", delimiter=",") 
+def validationAccuracy():
+	trainData = pd.read_csv("../Dataset/binarytrain.csv", delimiter=",") 
 
-attributeTrain = list(([row.SR, row.NR, row.TR, row.VR, row.SUR1, row.SUR2, row.SUR3, row.UR, row.FR, row.OR, row.RR, row.BR, row.MR, row.CR]) for row in trainData.itertuples())
-labelTrain = [(row.label) for row in trainData.itertuples()]
+	attributeTrain = list(([row.SR, row.NR, row.TR, row.VR, row.SUR1, row.SUR2, row.SUR3, row.UR, row.FR, row.OR, row.RR, row.BR, row.MR, row.CR]) for row in trainData.itertuples())
+	labelTrain = [(row.label) for row in trainData.itertuples()]
 
-testData = pd.read_csv("../Dataset/binarytest.csv", delimiter=",") 
+	testData = pd.read_csv("../Dataset/binarytest.csv", delimiter=",") 
 
-attributeTest = list(([row.SR, row.NR, row.TR, row.VR, row.SUR1, row.SUR2, row.SUR3, row.UR, row.FR, row.OR, row.RR, row.BR, row.MR, row.CR]) for row in testData.itertuples())
-labelTest = [(row.label) for row in testData.itertuples()]
-totalAccuracy = 0.0
+	attributeTest = list(([row.SR, row.NR, row.TR, row.VR, row.SUR1, row.SUR2, row.SUR3, row.UR, row.FR, row.OR, row.RR, row.BR, row.MR, row.CR]) for row in testData.itertuples())
+	labelTest = [(row.label) for row in testData.itertuples()]
+	totalAccuracy = 0.0
 
-svmClassifier = svm.SVC()
-svmClassifier = svmClassifier.fit(attributeTrain, list(labelTrain))
-#print(labelTrain[index])
-labelPredict = svmClassifier.predict(attributeTest)
-totalAccuracy = str(accuracyCalc(labelTest, labelPredict))
-print("Average accuracy: " + str(totalAccuracy))
+	svmClassifier = svm.SVC()
+	svmClassifier = svmClassifier.fit(attributeTrain, list(labelTrain))
+	#print(labelTrain[index])
+	labelPredict = svmClassifier.predict(attributeTest)
+	totalAccuracy = str(accuracyCalc(labelTest, labelPredict))
+	print("Average accuracy: " + str(totalAccuracy))
+
+def kFoldAccuracy():
+	data = pd.read_csv("../Dataset/binary.csv", delimiter=",")
+	kf = KFold(n_splits = len(data), random_state = None, shuffle = False)
+	totalAccuracy = 0.0
+	for train_index, test_index in kf.split(data):
+		trainData = data.iloc[train_index]
+		testData = data.iloc[test_index]
+		attributeTrain = list(([row.SR, row.NR, row.TR, row.VR, row.SUR1, row.SUR2, row.SUR3, row.UR, row.FR, row.OR, row.RR, row.BR, row.MR, row.CR]) for row in trainData.itertuples())
+		labelTrain = [(row.label) for row in trainData.itertuples()]
+		attributeTest = list(([row.SR, row.NR, row.TR, row.VR, row.SUR1, row.SUR2, row.SUR3, row.UR, row.FR, row.OR, row.RR, row.BR, row.MR, row.CR]) for row in testData.itertuples())
+		labelTest = [(row.label) for row in testData.itertuples()]
+		svmClassifier = svm.SVC()
+		svmClassifier = svmClassifier.fit(attributeTrain, labelTrain)
+		labelPredict = svmClassifier.predict(attributeTest)
+		print("Accuracy", end = ": ")
+		print(accuracyCalc(labelTest, labelPredict))
+		#labelPredict = svmClassifier.predict(attributeTrain)
+		#print("*" + str(accuracyCalc(labelTrain, labelPredict)))
+		totalAccuracy += accuracyCalc(labelTest, labelPredict)
+	totalAccuracy /= 189
+	print(totalAccuracy)
