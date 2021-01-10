24ed0a36d24311f05ee2f07a45ce9a2a655f8504
Jerry2001 <ngohongphuc2001@gmail.com>
2021-01-10 14:18:10 -0600

    Add ANN for abalone

--- Abalone/ANN/main.py
+++ Abalone/ANN/main.py
@@ -6,16 +6,14 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report 
 from sklearn.metrics import confusion_matrix 
 from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
 
 data = pd.read_csv("../Dataset/onehot.csv", delimiter=",")
 
 xTrain, xTest, yTrain, yTest = train_test_split(data.iloc[:, :-1], data.iloc[:, -1:], test_size=0.33, random_state= 0)
-treeClassifier = tree.DecisionTreeClassifier(max_depth = 12, max_features = "auto")
-treeClassifier.fit(xTrain, yTrain)
-yPredict = treeClassifier.predict(xTest)
-yTrainPredict = treeClassifier.predict(xTrain)
+network = MLPClassifier()
+network.fit(xTrain, yTrain)
+yPredict = network.predict(xTest)
+yTrainPredict = network.predict(xTrain)
 print(accuracy_score(yTest, yPredict) * 100)
 print(accuracy_score(yTrain, yTrainPredict) * 100)
-# file = "../Visualization/binary.dot"
-# tree.export_graphviz(treeClassifier, out_file=file, feature_names = data.columns[:-1], class_names = True, filled=True, rounded=True, special_characters=True)
-# call(['dot', '-Tpng', file, '-o', 'binary.png', '-Gdpi=600'])
